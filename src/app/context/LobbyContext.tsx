4761041b90917a5a75976d4f21161b7df1ab93ef
unknown <mateusz.lu123@gmail.com>
2025-08-17 10:45:00 +0200

    update lobby context to handle users

--- src/app/context/LobbyContext.tsx
+++ src/app/context/LobbyContext.tsx
@@ -1,11 +1,20 @@
 "use client";
 
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext } from "react";
 import type { ReactNode } from "react";
 
-type LobbyContextProps = {};
+import type { LobbyUserStats } from "~/types/lobby";
+import useArray from "../hooks/useArray";
 
-type LobbyUpdateProps = {};
+type LobbyContextProps = {
+  users: LobbyUserStats[];
+};
+
+type LobbyUpdateProps = {
+  addUser: (user: LobbyUserStats) => void;
+  removeUser: (userId: string) => void;
+  updateUser: (userId: string, updatedFields: Partial<LobbyUserStats>) => void;
+};
 
 const LobbyContext = createContext({} as LobbyContextProps);
 const LobbyUpdate = createContext({} as LobbyUpdateProps);
@@ -19,9 +28,31 @@ export function useUpdateLobby() {
 }
 
 export function LobbyProvider({ children }: { children: ReactNode }) {
+  const { array: users, push, removeByKey, updateObjectByKey } = useArray<LobbyUserStats>([]);
+
+  const addUser = (user: LobbyUserStats) => {
+    if (!users.find(u => u.id === user.id)) {
+      push(user);
+    }
+  };
+
+  const removeUser = (userId: string) => {
+    removeByKey("id", userId);
+  };
+
+  const updateUser = (userId: string, updatedFields: Partial<LobbyUserStats>) => {
+    const updates = Object.entries(updatedFields).map(([field, fieldValue]) => ({
+      field: field as keyof LobbyUserStats,
+      fieldValue,
+    }));
+    updateObjectByKey("id", userId, updates);
+  };
+
   return (
-    <LobbyContext.Provider value={{}}>
-      <LobbyUpdate.Provider value={{}}>{children}</LobbyUpdate.Provider>
+    <LobbyContext.Provider value={{ users }}>
+      <LobbyUpdate.Provider value={{ addUser, removeUser, updateUser }}>
+        {children}
+      </LobbyUpdate.Provider>
     </LobbyContext.Provider>
   );
 }
