cd27ebd9c6e2f4217b4eabec59a6a862af54820e
unknown <mateusz.lu123@gmail.com>
2025-08-17 11:29:30 +0200

    added more info to error and make it look nicer

--- src/app/_components/Lobby.tsx
+++ src/app/_components/Lobby.tsx
@@ -1,17 +1,23 @@
-"use client"
+"use client";
 
 import { useLobby } from "../context/LobbyContext";
 import LobbyTable from "./LobbyTable";
 
 const Lobby = () => {
-  const {  isError } = useLobby();
-
+  const { isError } = useLobby();
   if (isError) {
-    return <div className="p-4 text-red-500">Failed to load lobby data.</div>;
+    return (
+      <div className="rounded-md border border-red-500 bg-red-100 p-4 text-red-700 shadow-sm">
+        Failed to load lobby data. Only one instance of the same user/computer
+        can be in the lobby.
+      </div>
+    );
   }
-  return <div className="w-full flex justify-center">
-    <LobbyTable/>
-  </div>;
+  return (
+    <div className="flex w-full justify-center">
+      <LobbyTable />
+    </div>
+  );
 };
 
 export default Lobby;
